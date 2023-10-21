8dfb9041c0610a5da0078b771a178d035d43749b
IT-Arkhipov <it.arkhipov@gmail.com>
2023-10-21 18:33:08 +0300

    code refactored for optimal selectors

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -7,8 +8,18 @@ def browser_config():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-gpu')
+    options.add_argument('--disable-notifications')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--enable-automation')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-setuid-sandbox')
+    browser.config.driver_options = options
     browser.config.base_url = "https://demoqa.com"
-
 
     yield
 
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,37 +1,40 @@
 import os
 from selene import browser, have, command
-
+from selenium.webdriver.common.keys import Keys
 
 def test_demoqa_complete_form():
     browser.open("/automation-practice-form")
 
     browser.element('#firstName').type('FirstName')
     browser.element('#lastName').type('LastName')
+
     browser.element('#userEmail').type('mymail@test.ru')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type('9171234567')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click().type('June')
-    browser.element('.react-datepicker__year-select').click().type('1985')
-    browser.element('.react-datepicker__day--025').click()
+    browser.element('#genterWrapper').all('label').element_by(have.text('Male')).click()
+    browser.element('#userNumber').type('9170770905')
+    browser.element('#dateOfBirthInput').send_keys(Keys.CONTROL + 'a').send_keys('11 Oct 2023').press_enter()
     browser.element('#subjectsInput').type('Maths').press_enter()
-    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('#hobbiesWrapper').perform(command.js.scroll_into_view)
+    browser.element('#hobbiesWrapper').all('label').element_by(have.text('Sports')).click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('img/sample.jpg'))
     browser.element('#currentAddress').type('My current address')
     browser.element('#state').perform(command.js.scroll_into_view).click()
     browser.element('#react-select-3-input').type('NCR').press_enter()
     browser.element('#city').click()
     browser.element('#react-select-4-input').type('Delhi').press_enter()
-    browser.element('#submit').click()
+    browser.element('footer').execute_script('element.remove()')
+    browser.element('#submit').perform(command.js.click)
 
     browser.element('.table-responsive').all('td:nth-of-type(2)').should(have.texts(
         'FirstName LastName',
         'mymail@test.ru',
         'Male',
-        '9171234567',
-        '23 June,1985',
+        '9170770905',
+        '11 October,2023',
         'Maths',
-        'Reading',
+        'Sports',
         'sample.jpg',
         'My current address',
         'NCR Delhi'))
+
+
+# browser.execute_script('document.querySelector("#fixedban").remove()')
